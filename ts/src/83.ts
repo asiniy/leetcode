cccb96c7a5346602339c5bfdeeeca8f599b5ceb3
Alex Antonov <kaburbundokel11g@inbox.ru>
2024-09-15 14:06:58 +0500

    83 - faster

--- ts/src/83.ts
+++ ts/src/83.ts
@@ -11,28 +11,45 @@
  */
 
 // @ts-ignore
+// function deleteDuplicates(head: ListNode | null): ListNode | null {
+//   if (!head) { return null }
+//   const set = new Set<number>()
+
+//   // @ts-ignore
+//   let chainNode: ListNode = head
+//   // @ts-ignore
+//   let node: ListNode = head.next
+
+//   set.add(chainNode.val)
+
+//   while (node) {
+//       if (!set.has(node.val)) {
+//           set.add(node.val)
+//           chainNode.next = node
+//           chainNode = node
+//       }
+
+//       node = node.next
+//   }
+
+//   chainNode.next = null
+
+//   return head
+// };
+
+// A better solution - I overlooked it
+// @ts-ignore
 function deleteDuplicates(head: ListNode | null): ListNode | null {
-  if (!head) { return null }
-  const set = new Set<number>()
+  let current = head
 
-  // @ts-ignore
-  let chainNode: ListNode = head
-  // @ts-ignore
-  let node: ListNode = head.next
-
-  set.add(chainNode.val)
-
-  while (node) {
-      if (!set.has(node.val)) {
-          set.add(node.val)
-          chainNode.next = node
-          chainNode = node
+  while (current && current.next) {
+      if (current.val === current.next.val) {
+          current.next = current.next.next
+          continue
       }
 
-      node = node.next
+      current = current.next
   }
-
-  chainNode.next = null
 
   return head
 };
